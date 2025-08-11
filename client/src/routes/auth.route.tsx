86571636091a7bf15b7424f5550e1d72630f8edf
nikhilmahato21 <nikhilmahato3337@gmail.com>
2025-08-11 23:25:29 +0530

    prevent logged-in users from accessing the auth routes

--- client/src/routes/auth.route.tsx
+++ client/src/routes/auth.route.tsx
@@ -1,7 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { DashboardSkeleton } from "@/components/skeleton-loaders/dashboard-skeleton";
+import useAuth from "@/hooks/api/use-auth";
+import { Navigate, Outlet } from "react-router-dom";
 
 const AuthRoute = () => {
-  return <Outlet />;
+  const{data:authData,isLoading}=useAuth();
+  const user = authData?.user;
+
+if(isLoading) return <DashboardSkeleton />; // Optionally, you can show a loading spinner here
+
+  if(!user) return <Outlet />;
+  return <Navigate to={`/workspace/${user.currentWorkspace?._id}`} replace />;
 };
 
 export default AuthRoute;
